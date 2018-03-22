f1624e073deb1fdb6450a4e44937551b46327089
Sujata Tamang <sujata.tamang@protonmail.ch>
2018-03-22 12:39:49 +0100

    contextualization

--- contracts/endorsement.sol
+++ contracts/endorsement.sol
@@ -1,20 +1,76 @@
 pragma solidity ^0.4.0;
 
 contract Endorsement { 
-	address public endorser;
-	mapping (address => uint) public edsValues;
 
+	struct Endorsee {
+		string name;
+		uint impact;
+		uint nER;
+		//uint trustScore;
+		// endorse id only or subset of info ??
+	}
+	struct Endorser {
+		uint nEG; 
+		uint ei; 
+	}
+	
+	address public owner;
+//	address public endorser;
+//	address public endorsee;
+	address public arbiter; //maybe an insurer in the middle??
+	mapping (address => Endorser) public endorsers;
+	Endorsee[] public endorsees;
+
+	mapping (address => uint) public edsValues;
+	mapping (address => uint) public ei;
+	uint public TOTAL = 3000000000000000000; 
+	uint public iep = 1;
+	uint public nEG = 0;
+	
 	//event to notify when transfer is complete
 	event Delivered(address from, address to, uint amount);
 
-	function Endorsement() {
-		endorser = msg.sender;
+	function Endorsement (string _name, string name1, string name2 ) {
+		owner = msg.sender;
+		name = _name
+
+		endorsees.pushE(Endorsee({name: name1, nER:0 } ));
+		endorsees.pushE(Endorsee({name: name2, nER:0 } ));
+
 	}
 
-	function create(address receiver, uint amount) { 
-		if (msg.sender != endorser) return;
-		edsValues[receiver]	+= amount;
+	//function authorize to eliminate given endorsement 
+	//in future if someone doesn't want to trust anymore
+	function authorize() { 
+		require(msg.sender == endorser);
+		nEG[msg.sender] = 1;
+		impact[msg.sender] = impact //formula of impact can be used  again here.
 	}
+
+
+
+	function create() payable {
+		require(msg.value > 0 && msg.value % TOTAL == 0);
+		edsValues[receiver] += (msg.value / TOTAL);
+	}
+
+	function endorse(address receiver)  { 
+//		require(msg.value > 0) 
+//		edsValues[msg.sender] += 
+		
+		if (msg.sender != endorser) return;
+		//keep track of sender address from where eds is given to have no of connection
+		nEG = nEG + 1;
+//		edsValues[receiver]	+= 1/nEG;
+		ei[receiver] += (nEG/250) * edsValues[receiver];
+		//nEG/250 * Rp  * min(G,R)/max(G,R)
+	}
+
+
+	//function to store all the endorsers for a given participant. 
+	//ex: A,B,C,D endorses E 
+	//for (i=0; i < endorser.length; i++)
+	//endorsers = endorser[i].name, endorser[i].nER to compute the impact
 
 	function transfer(address receiver, uint amount) {
 		if (edsValues[msg.sender] < amount) return;
