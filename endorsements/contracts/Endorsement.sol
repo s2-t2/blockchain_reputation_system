06514b24af16bdbe61806ddea39817e9b6c859b8
Sujata Tamang <sujata.tamang@protonmail.ch>
2018-04-04 15:41:40 +0200

    track connections

--- endorsements/contracts/Endorsement.sol
+++ endorsements/contracts/Endorsement.sol
@@ -11,23 +11,22 @@ contract Endorsement is EDSToken {
 		//uint id;
 		address endorsee;
 		string nameEndorsee;
-		//uint nER;
-//		uint nEG;
-//		uint nER;
+	//	uint [] givenTo;
 	}
 
 	struct Endorsee { 
-		uint id;
-		string nameEndorser;
-		address endorsee;
-		address endorsers;
-//		uint nEG;
-//		uint nER;
+		//uint id;
+		//string nameEndorser;
+		address endorser;
+	//	uint [] receivedFrom;
+
 	}
 
 	struct Trust { 
 		address user;
+		uint ratio;
 		uint usedPower;
+		uint receivedEDS;
 		uint impact;
 		uint totalImpact;
 	}
@@ -71,6 +70,16 @@ contract Endorsement is EDSToken {
 
 	//EDSToken (2100000, "EDS", "%" );
 
+	struct TrackConnection { 
+		address givenTo;
+		address receivedFrom;
+	}
+
+	TrackConnection [] public connections; 
+
+//	mapping (address => trackConnection ) To;
+//	mapping (address => receivedFrom[] ) From; 
+
 
 	// send endorsement
 	function sendEndorsement(address _endorsee, string _name) public payable { 
@@ -78,24 +87,33 @@ contract Endorsement is EDSToken {
 		require (msg.sender != _endorsee );
 		
 		endorsee = _endorsee;
+		address endorser = msg.sender;
 		name = _name;
 		nEG[ msg.sender ] = nEG[ msg.sender ] + 1 ;
 
 		endorsers[msg.sender ] = Endorser({
 			endorsee: endorsee,
 			nameEndorsee: name
-			//nER: 
 		});
 
-//		if (nEG [msg.sender] == 1) { 
-//			EDSTransfer(msg.sender, 300 );
-//		} 
+		nER[ _endorsee] = nER[ _endorsee] + 1;
 
+		TrackConnection memory newConnections = TrackConnection({
+			givenTo: endorsee,
+			receivedFrom: endorser 
+		});
 
-		//EDSTransfer ( msg.sender, 100 );
-		nER[endorsee ] = nER[endorsee ] + 1;
+		connections.push(newConnections );
+	}
+
+	function ReceivedEDS ( address _user) public returns (uint _RE ) {
+		//for i in list  of endorsees of user, 
+		//get the used power of each of them and add all together.
+
 
 	}
+	
+	//some helper functions for calculations
 
 	function Division( uint _numerator, uint _denominator, uint _precision) public constant returns (uint _quotient) {
 		uint numerator = _numerator * 10 ** (_precision + 1);
@@ -104,9 +122,33 @@ contract Endorsement is EDSToken {
 		return (quotient);		
 	}
 
+	function max (uint x, uint y ) public constant returns (uint) {
+		if (x < y) {
+			return y;
+		} else  {  
+			return x;
+		}
+	}
+
+	function min (uint x, uint y ) public constant returns (uint) { 
+		if (x < y) { 
+			return x;
+		} else {
+			return y;
+		}
+	}
+
 	//compute trust score
+	//supports decimal upto 9 place
+	//can convert to decimal by dividing all values with 1e9 and get the exact score on 
+	//client side
 	function computeTrust ( address _user ) { 
-		//nEG
+//		user = _user;
+//		ratio = Division( ( nEG[  _user] ), ( nER[ _user ] ), 9 );
+//		usedPower = Division(1, (nEG[_user ]), 9 );
+//		receivedEDS = ReceivedEDS( _user );
+	//	totalImpact = ratio + up + RE;  
+
 	}
 
 }
