702bd345df16c835eaff33ad6ee9122f573ff85c
Sujata Tamang <sujata.tamang@protonmail.ch>
2018-05-11 20:41:59 +0200

    nr of incoming and outgoing connections working

---
+++ graph/bitcoindata.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import csv
+
+from pandas import read_csv
+
+#df = read_csv('soc-sign-bitcoinalpha.csv')
+#df.columns=['source','target','rating','time']
+#df.to_csv('soc-sign-bitcoinalpha-label.csv')
+
+myDict = {}
+source = []
+target = []
+
+nER = {}
+with open('./testfile.csv', newline='' ) as File : 
+    #reader = csv.reader(File)
+    reader = csv.DictReader(File)
+    for row in reader:
+        source.append(row['source'] )
+        target.append(row['target'])
+        #print (row['source'], row['target'])
+        #key = row['source']
+        #value = row['target']
+
+        #myDict[key].append(value)
+
+
+uniqueSource = []
+uniqueTarget = []
+
+for i in source : 
+    if i not in uniqueSource :
+        uniqueSource.append(i)
+for i in target: 
+    if i not in uniqueTarget:
+        uniqueTarget.append(i)
+
+nER =  ({key: None for key in uniqueSource } )
+#nEG = {}
+for i in range(0,len(source)) :
+    counter = 0;
+    for j in range(0, len(target) ):
+        if source[i] in target[j] :
+            counter = counter + 1
+            #nEG = nEG + {source[i]: target[j] }
+            nER[(source[i])] = counter;
+
+nEG = ({key: None for key in uniqueTarget } )
+for i in range(0,len(target)) :
+    counter =0 
+    for j in range(0, len(source ) ):
+        if target[i] in source[j] :
+            counter = counter + 1;
+            nEG[target[i] ] = counter
+        #if source[i] 
+        #if source[i] == target [j] :
+
+print (source)
+print (target)
+print (uniqueSource)
+print (uniqueTarget)
+print (nER)
+print (nEG)
+
--- graph/friends.py
+++ graph/friends.py
@@ -2,12 +2,13 @@
 
 import json,jsonlines
 
+
 with open('./smalluser.json') as json_file:
     data = json.load(json_file)
-    for i in range(0,len(data) ):
+    #for i in range(0,len(data) ):
         #print (data[i]['user_id'])
         #print ("friends of " + data[i]['name'] +  "with user id" + data[i]['user_id']  )
-        print (data[i]['friends'])
+        #print (data[i]['friends'])
 
 
 
