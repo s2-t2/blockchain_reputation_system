4b41afb2ed158bdfa5005b1ae85a42ee95b6aea9
Sujata Tamang <sujata.tamang@protonmail.ch>
2018-09-13 21:08:57 +0200

    remove unnecessary file

--- contracts/participants.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.0;
-
-contract Participants { 
-	// mapping for the participants info and address
-
-
-
-}
--- contracts/simpleEds.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.4;
-
-contract SimpleEds {
-
-	//how much endorsement power each address has used
-	mapping(address => uint) public endorsement 
-}
